87b8377d2ba020a813cd61c29aaec2636ba5cb8d
jony12004 <alexsapu22@gmail.com>
2025-09-24 14:21:41 +0200

    Primera clase de fundamentos

--- FundamentosJAVA/src/App.java
+++ FundamentosJAVA/src/App.java
@@ -1,5 +1,52 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static  boolean logico = true;
+    final static double PI = 3.14592;
+    
+    public static  void main(String[] args) throws Exception {
+        //  (TIPO) IDENTIFICADOR = VALOR
+        String cadena = "Me llamo Jonathan";
+        int edad = 21;
+        double salario = 1500.50;
+        System.out.println(salario);
+        int x = 5;  int y = 7; int z;
+        int xx = 5, yy = 7, zz = 10;
+
+        int variable=0;
+        System.out.println(variable);
+
+        variable = 33;
+        System.out.println(variable);
+
+        int valor = 1/3;
+        System.out.println(valor);
+        double valor2 = 1/3.0;
+        System.out.println(valor2);
+        int a = (int) 2.6;
+        System.out.println(a);
+        
+        boolean logico = false; //False
+        boolean logico2 = false;
+        
+        System.out.println(logico);
+        System.out.println(App.logico);
+
+        System.out.println(PI);
+
+        int b = 'b';
+        System.out.println(b);
+        char c = 98;
+        System.out.println(c);
+    }
+    /*  Definicion de la funcion funcion
+    no devuelve nada*/
+    /**
+     * Programa para imprimir texto
+     * @author Jonathan Sanchez Puglla
+     * @param texto a imprimir
+     * @version 1.0
+     */
+    public static void funcion() {
+        boolean logico = false; 
     }
 }
+
